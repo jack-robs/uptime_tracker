fdf52888ccaa5a8af4cc03de5d90e79c0aa20bf4
Jack Robertson <johnr00@protonmail.com>
2019-02-13 13:50:53 -0500

    refactor ping_req

---
+++ ping_request.py
@@ -0,0 +1,22 @@
+#12Feb19
+#ping website, get status code
+
+
+import requests
+
+def ping(url):
+    r = requests.get(url)
+    status = r.status_code
+    return status
+   
+
+
+url = 'https://news.ycombinator.com'
+
+status = ping(url)
+
+status_codes = []
+status_codes.append(status)
+
+print(status_codes)
+
